17c3abf3a894444f93fa4e0a3a054327e3de5786
kyawthura-gg <ggaryal@gmail.com>
2020-12-01 20:04:18 +0630

    updated server auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -25,7 +25,9 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
-
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 const User = mongoose.model("User", userSchema);
 
 export default User;
